c8345c715118481465ec2ca882a80ed7d3946cb1
Shane Cory Selig <shanecs@me.com>
2022-02-09 09:45:28 -0600

    Updated dependencies

--- server/src/modules/prisma/prisma.service.ts
+++ server/src/modules/prisma/prisma.service.ts
@@ -1,9 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
-
-import { UserListener } from '../user/user.listener';
-
 import { PRISMA_CLIENT_OPTIONS } from './prisma.config';
+
 
 @Injectable()
 export class PrismaService
@@ -19,8 +17,6 @@ export class PrismaService
     this.$on('error', (_e) => {
       // Do something
     });
-
-    this.$use(UserListener.onCreated);
   }
 
   async onModuleDestroy() {
